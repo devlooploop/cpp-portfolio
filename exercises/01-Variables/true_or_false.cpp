adbb79adff2b2abe08a4e5caf42fac7aa2368471
devloop <mychannelmine2@gmail.com>
2025-11-13 23:39:47 +0100

    Added more #3 to exercises/00-Input-Output/ & 01-Variables/

---
+++ exercises/01-Variables/true_or_false.cpp
@@ -0,0 +1,56 @@
+// Introduction to Programming Using C++ - Level 1
+ 
+// Lesson #24 Relational Operators
+// exampl #1 from the video
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A = 10, B = 20;
+
+
+    cout << (A == B) << endl;
+
+    cout << (A != B) << endl;
+
+    cout << (A > B) <<  endl;
+
+    cout << (A < B) <<  endl;
+
+    cout << (A >= B) << endl;
+
+    cout << (A <= B) << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
